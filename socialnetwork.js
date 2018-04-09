c810199a018df5ddeb72e2ed9742705e1c8ec2e2
Josh Curl <josh@curl.me>
2018-04-08 21:56:28 -0700

    More nice things

--- socialnetwork.js
+++ socialnetwork.js
@@ -1,14 +1,33 @@
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
+function getPosts() {
+    var posts = []
+
+    var articles = document.querySelectorAll('.story_body_container')
+    for (var i = 0; i < articles.length; i++) {
+        var article = articles[i]
+
+        var poster = getPoster(article)
+        var recipient = getRecipient(article)
+
+        var footer = article.parentElement.querySelector('footer')
+
+        var like = getLike(footer)
+        var comment = getLike(footer)
+
+        var rawText = article.childNodes[1].innerText
+
+        posts.push({
+            'poster': poster,
+            'recipient': recipient,
+            'raw_text': rawText,
+            'like': like,
+            'comment': comment
+        })
+    }
+
+    return posts
 }
 
-var posts = []
-
-var articles = document.querySelectorAll('.story_body_container')
-for (i = 0; i < articles.length; i++) {
-    var article = articles[i]
-
+function getPoster(article) {
     var style = article.querySelector('i').getAttribute('style')
     var pictureUrl = style.split('\'')[1]
     pictureUrl = pictureUrl.replaceAll('\\3a ', ':')
@@ -18,17 +37,57 @@ for (i = 0; i < articles.length; i++) {
     var firstStrong = article.querySelector('strong')
     var name = firstStrong.innerText
 
-    var rawText = article.childNodes[1].innerText
-
-    posts.push({
-        "poster": {
-            "name": name,
-            "picture_url": pictureUrl
-        },
-        "raw_text": rawText
-    })
+    return {
+        'name': name,
+        'picture_url': pictureUrl
+    }
 }
 
-window.Viewer.print(JSON.stringify({
-    "posts": posts
-}))
+function getRecipient(article) {
+    var recipient = {}
+    var firstSpan = article.querySelector('span')
+    var spanLinks = firstSpan.querySelectorAll('a')
+    for (var i = 0; i < spanLinks.length; i++) {
+        var spanLink = spanLinks[i]
+        if (i == spanLinks.length - 1) {
+            var href = spanLink.getAttribute('href')
+            if (href.startsWith('/groups/')) {
+                recipient = {
+                    'type': 'group',
+                    'name': spanLink.innerText,
+                    'url': href
+                }
+                break
+            }
+        }
+    }
+    return recipient
+}
+
+function getLike(footer) {
+    return getFooterItem(footer, 'Like')
+}
+
+function getComment(footer) {
+    return getFooterItem(footer, 'Comment')
+}
+
+function getFooterItem(footer, itemText) {
+    var item = {}
+    var footerLinks = footer.querySelectorAll('a')
+    for (var i = 0; i < footerLinks.length; i++) {
+        var footerLink = footerLinks[i]
+        if (footerLink.innerText.includes(itemText)) {
+            item = {
+                'id': footerLink.getAttribute('id')
+            }
+            break
+        }
+    }
+    return item
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this
+    return target.split(search).join(replacement)
+}
